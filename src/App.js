def443e64a6ff2ec454d9d898aa0b1beeb74979d
mynameSumin <msm4167@naver.com>
2024-07-25 15:19:12 +0900

    chore : 불필요한 코드 삭제

--- src/App.js
+++ src/App.js
@@ -1,49 +1,24 @@
 // src/App.js
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Auth from './components/Auth';
-import Login from './components/Login';
-import MainPage from './components/MainPage';
-import { GoogleOAuthProvider } from '@react-oauth/google';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Auth from "./components/Auth";
+import Login from "./components/Login";
+import MainPage from "./components/MainPage";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const App = () => {
-    return (
-        <GoogleOAuthProvider clientId="">
-            <Router>
-                <Routes>
-                    <Route path="/auth" element={<Auth />} />
-                    <Route path="/mainpage" element={<MainPage />} />
-                    <Route path="/" element={<Login />} />
-                    {/* 필요한 다른 경로들도 설정 */}
-                </Routes>
-            </Router>
-        </GoogleOAuthProvider>
-    );
+  return (
+    <GoogleOAuthProvider clientId="">
+      <Router>
+        <Routes>
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/mainpage" element={<MainPage />} />
+          <Route path="/" element={<Login />} />
+          {/* 필요한 다른 경로들도 설정 */}
+        </Routes>
+      </Router>
+    </GoogleOAuthProvider>
+  );
 };
 
 export default App;
-
-
-// // src/App.js
-// import React, { useState } from 'react';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import Auth from './components/Auth';
-// import Login from './components/Login';
-// import MainPage from './components/MainPage';
-
-// const App = () => {
-//     const [accessToken, setAccessToken] = useState(null);
-
-//     return (
-//         <Router>
-//             <Routes>
-//                 <Route path="/auth" element={<Auth setAccessToken={setAccessToken} />} />
-//                 <Route path="/mainpage" element={<MainPage accessToken={accessToken} />} />
-//                 <Route path="/" element={<Login />} />
-//                 {/* 필요한 다른 경로들도 설정 */}
-//             </Routes>
-//         </Router>
-//     );
-// };
-
-// export default App;
