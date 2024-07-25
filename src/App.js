8ba561ad6d7cb11316df01b610622e7b726ab052
mynameSumin <msm4167@naver.com>
2024-07-25 15:39:45 +0900

    .env gitignore에 추가

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Auth from "./components/Auth";
 import Login from "./components/Login";
-import MainPage from "./components/MainPage";
+import MainPage from "./pages/MainPage";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const App = () => {
@@ -13,7 +13,9 @@ const App = () => {
         <Routes>
           <Route path="/auth" element={<Auth />} />
           <Route path="/mainpage" element={<MainPage />} />
-          <Route path="/" element={<Login />} />
+          {/* <Route path="/" element={<Login />} /> */}
+          {/* 경로 임시 설정 */}
+          <Route path="/" element={<MainPage />} />
           {/* 필요한 다른 경로들도 설정 */}
         </Routes>
       </Router>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,43 +1,47 @@
 // src/components/Login.js
-import React from 'react';
-import { Button } from '@mui/material';
-import { KAKAO_AUTH_URL } from '../utils/OAuth';
-import { GoogleLogin } from '@react-oauth/google';
-import { jwtDecode } from 'jwt-decode';
-import axios from 'axios';
+import React from "react";
+import { Button } from "@mui/material";
+import { GoogleLogin } from "@react-oauth/google";
+import { jwtDecode } from "jwt-decode";
+import axios from "axios";
 
 const Login = () => {
-    const handleGoogleLoginSuccess = async (response) => {
-        const decoded = jwtDecode(response.credential);
-        console.log('Google Login Success:', decoded);
+  const handleGoogleLoginSuccess = async (response) => {
+    const decoded = jwtDecode(response.credential);
+    console.log("Google Login Success:", decoded);
 
-        // 서버로 Google 토큰을 전송하여 인증 처리
-        try {
-            const res = await axios.post('http://localhost:3000/auth', {
-                token: response.credential
-            });
-            console.log('Server response:', res.data);
-        } catch (error) {
-            console.error('Server error:', error);
-        }
-    };
+    // 서버로 Google 토큰을 전송하여 인증 처리
+    try {
+      const res = await axios.post("http://localhost:3000/auth", {
+        token: response.credential,
+      });
+      console.log("Server response:", res.data);
+    } catch (error) {
+      console.error("Server error:", error);
+    }
+  };
 
-    const handleGoogleLoginFailure = (error) => {
-        console.error('Google Login Failed:', error);
-    };
+  const handleGoogleLoginFailure = (error) => {
+    console.error("Google Login Failed:", error);
+  };
 
-    return (
-        <div>
-            <Button onClick={() => {
-                window.location.href = KAKAO_AUTH_URL;
-            }} variant="contained" color="primary">카카오로 로그인하기</Button>
-            <GoogleLogin
-                onSuccess={handleGoogleLoginSuccess}
-                onError={handleGoogleLoginFailure}
-            />
-        </div>
-    );
+  return (
+    <div>
+      <Button
+        onClick={() => {
+          window.location.href = process.env.REACT_APP_KAKAO_AUTH_URL;
+        }}
+        variant="contained"
+        color="primary"
+      >
+        카카오로 로그인하기
+      </Button>
+      <GoogleLogin
+        onSuccess={handleGoogleLoginSuccess}
+        onError={handleGoogleLoginFailure}
+      />
+    </div>
+  );
 };
 
 export default Login;
-
