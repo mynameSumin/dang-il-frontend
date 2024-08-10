ef596486714de7d3c5d8a109e4a661412c4bda69
ikjun <chj3123@naver.com>
2024-08-10 17:34:07 +0900

    feat : Login.js에 카카오 소셜로그인 기능 추가

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,31 +2,49 @@ import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
-  const navigate = useNavigate(); //navigate를 사용하여 페이지를 이동할수있다.
+  const navigate = useNavigate();
 
   useEffect(() => {
     // URL에서 인증 코드 추출
     const params = new URLSearchParams(window.location.search);
-    const code = params.get('code');
+    const googleCode = params.get('code');
+    const kakaoCode = params.get('kakao_code'); // Kakao의 경우 인증 코드를 kakao_code로 받는다고 가정
 
-    if (code) {
-      // 인증 코드를 FastAPI 서버로 전송
+    if (googleCode) {
+      // Google 인증 코드를 FastAPI 서버로 전송
       fetch(`https://www.dangil-artisticsw.site/auth/google/callback`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ code: code })
+        body: JSON.stringify({ code: googleCode })
       })
         .then(response => response.json()) // fetch 요청이 완료되면 응답이 response에 담김. response안의 json이란 함수를 실행하여 json형식으로 응답을 파싱함.
-        // function (a,b){ return a+b} = (a,b) => a+b
         .then(data => {
-          console.log('Token and user data:', data);
+          console.log('Google Token and user data:', data);
           localStorage.setItem('token', data.id_token);
           navigate('/');
         })
         .catch(error => {
-          console.error('Error:', error);
+          console.error('Google Login Error:', error);
+        });
+    } else if (kakaoCode) {
+      // Kakao 인증 코드를 FastAPI 서버로 전송
+      fetch(`https://www.dangil-artisticsw.site/auth/kakao/callback`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ code: kakaoCode })
+      })
+        .then(response => response.json())
+        .then(data => {
+          console.log('Kakao Token and user data:', data);
+          localStorage.setItem('token', data.id_token);
+          navigate('/');
+        })
+        .catch(error => {
+          console.error('Kakao Login Error:', error);
         });
     } else {
       console.error('No authorization code found');
@@ -37,3 +55,4 @@ const Login = () => {
 };
 
 export default Login;
+
