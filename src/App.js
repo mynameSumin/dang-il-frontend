deb4d4488cb17a329ff999f9f1587688ba9901a1
ikjun <chj3123@naver.com>
2024-08-15 18:01:16 +0900

    feat: 팝업창 블러처리 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LoginGoogle from './components/LoginGoogle';
+import LoginKakao from './components/LoginKakao';
+import MainPage from './pages/MainPage';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainPage />} /> 
+        <Route path="/auth/google/callback" element={<LoginGoogle />} />
+        <Route path="/auth/kakao/callback" element={<LoginKakao />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
+
+//1. npm start하면 위의 route에서 path="/"에 해당하는 mainpage로 이동함.
+
