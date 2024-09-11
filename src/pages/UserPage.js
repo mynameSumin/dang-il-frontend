9a302e7b1d1e889fbc370ff82483e1af1f932f8a
ikjun <chj3123@naver.com>
2024-09-11 18:45:10 +0900

    feat : 개인페이지 메뉴 버튼 기능 추가 및 최대 id 도달했을때 화살표 연하게 디자인

--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -1,12 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import Clock from '../components/Clock.js';
 import "../styles/UserPage.css";
+import { FiEdit } from "react-icons/fi"
 
 const UserPage = () => {
   const { userId } = useParams(); // URL에서 userId를 받아옴
   const navigate = useNavigate();
-  const minUserId = 0; // 최소 사용자 ID 설정 (1로 변경)
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false); // 드롭다운 상태 추가
+  const dropdownRef = useRef(null); // 드롭다운 메뉴의 참조
+  const minUserId = 0; // 최소 사용자 ID 설정 
   const maxUserId = 8; // 최대 사용자 ID 설정
 
   // 이전 사용자로 이동하는 함수
@@ -30,6 +33,28 @@ const UserPage = () => {
     navigate('/'); // 메인 페이지로 이동
   };
 
+  const toggleDropdown = (e) => {
+    e.stopPropagation(); // 이벤트 전파 방지
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  useEffect(() => {
+    //화면 바깥 클릭하면 토글 목록 닫힘
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setIsDropdownOpen(false);
+      }
+    };
+
+    // 전역 클릭 이벤트 등록
+    document.addEventListener("click", handleClickOutside);
+
+    // 컴포넌트 언마운트 시 이벤트 제거
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, [isDropdownOpen]);
+
   return (
     <div style={{ padding: '20px' }}>
       <h1>User Page</h1>
@@ -44,26 +69,33 @@ const UserPage = () => {
           <button
             onClick={handlePrevUser}
             disabled={parseInt(userId) === minUserId}
-            className={`control-button ${parseInt(userId) === minUserId ? 'disabled' : ''}`}
+            className={`control-button ${parseInt(userId) === minUserId ? 'invisible' : ''}`}
           >
             <span className="icon">＜</span>
           </button>
           {/* 중간 기능 버튼 */}
-          <button className="control-button">
-            <span className="icon">M</span>
+          <button className="control-button"> 
+            <span className="icon">M</span> 
           </button>
-          <button className="control-button">
-            <span className="icon">＜/＞</span>
-          </button>
+          <span className={"dropdown-button"} onClick={toggleDropdown}>
+            <FiEdit/>
+          </span>
+            <div className={`dropdown-content ${isDropdownOpen ? 'active' : ''}`} ref={dropdownRef}>
+              <button>책</button>
+              <button>PDF</button>
+              <button>스탠드</button>
+              <button>메모</button>
+            </div>
           {/* 다음 사용자로 이동하는 버튼, 최대 사용자 ID를 초과하면 비활성화 */}
           <button
             onClick={handleNextUser}
             disabled={parseInt(userId) === maxUserId}
-            className={`control-button ${parseInt(userId) === maxUserId ? 'disabled' : ''}`}
+            className={`control-button ${parseInt(userId) === maxUserId ? 'invisible' : ''}`}
           >
             <span className="icon">＞</span>
           </button>
         </div>
+        
         {/* 메인 페이지로 이동하는 버튼 */}
         <button onClick={handleGoHome} className="home-button">Go to Home</button>
       </div>
