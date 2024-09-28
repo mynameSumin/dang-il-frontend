df9deada7f699d66e17bf845f882aca79296e6d1
ikjun <chj3123@naver.com>
2024-09-28 19:17:28 +0900

    chore : 책이름 관련 기능 추가

--- src/components/Book.js
+++ src/components/Book.js
@@ -2,64 +2,107 @@ import React, { useState, useRef, useEffect } from 'react';
 import "../styles/Book.css";
 import { RiHomeLine } from "react-icons/ri";
 import { RiDownloadLine } from "react-icons/ri";
-// import { RxPencil1 } from "react-icons/rx";
 import { LuEraser } from "react-icons/lu";
 import { LiaHighlighterSolid } from "react-icons/lia";
 import { FaRegFile } from "react-icons/fa6";
 import icon from "../assets/+ icon.png";
 import Ellipse from "../assets/Ellipse 2.png";
 import Vector from "../assets/Vector.png";
-
-
-
+import { useCookies } from "react-cookie";
 
 const Book = ({editBook, setEditBook}) => {
+  const [cookies] = useCookies(["session_id"]);
   // 편집 모드 상태 (기본값은 false로 비편집 상태)
-  const [isEditing, setIsEditing] = useState(false);
-  // 책 이름 상태
-  const [bookName, setBookName] = useState("Book Name");  
-  const bookRef = useRef(null);
+  const [NameEditing, setNameEditing] = useState(false);
 
-  const [previousBookName, setPreviousBookName] = useState(bookName);
   // 책 내용 저장
   const [noteDescription, setNoteDescription] = useState('');
 
-  //+버튼 상태
+  // +버튼 상태
   const [plusButton, setPlusButton] = useState(false);
 
- // 입력값을 상태에 저장
-  const handleChange = (event) => {
-    setNoteDescription(event.target.value);
-  };
-
-  const handleBookClick = (e) => {
+  // 책 클릭시 편집화면 활성화
+  const bookImageClick = (e) => {
     e.stopPropagation(); 
-    setEditBook(!editBook); // 책 편집 화면을 활성화
+    setEditBook(!editBook);
   };
 
-  // p 태그 클릭 시 편집 모드로 전환
-  const handleEditClick = (e) => {
-    e.stopPropagation(); // 추가: 이벤트 전파를 멈춤
-    setIsEditing(true);
-  };
 
-  // input 필드에서 엔터 키 또는 포커스를 잃었을 때 호출될 함수
-  const handleInputChange = (e) => {
+  // 책이름 관련 start
+
+  // 책 이름 상태
+  const [bookName, setBookName] = useState("Book Name");  
+  const bookRef = useRef(null);
+  const inputRef = useRef(null); 
+  const textareaRef = useRef(null); // input활성화됐을때 enter나 esc로만 input나갈수있음
+  // 책이름 클릭 시 편집 모드로 전환
+  const bookNameEdit = (e) => {
+    e.stopPropagation();
+    setNameEditing(true);
+  };
+  // 책이름 변경될때마다 호출되는 함수
+  const bookNameChange = (e) => {
     e.stopPropagation(); // 추가: 이벤트 전파를 멈춤
     setBookName(e.target.value);
   };
-
-  const handleInputKeyDown = (e) => {
-    e.stopPropagation(); // 추가: 이벤트 전파를 멈춤
-    if(bookName=="") return
-    if (e.key === 'Enter' || e.key === 'Escape') {
-      setIsEditing(false);
-      createBook(bookName);
-      updateBook(bookName, "설명을 여기에 입력");
+  // 책이름 변경되고 엔터누르면 호출
+  const bookNameEnter = async (e) => {
+    e.stopPropagation(); // 이벤트 전파를 멈춤
+    if (bookName === "") return; // bookName이 비어있으면 함수 종료
+    if (e.key === 'Enter' || e.key === 'Escape' /*Escape은 Esc를 의미함*/) {
+      setNameEditing(false); // 편집 상태 종료
+  
+      // GET 요청을 보냅니다.
+      try {
+        const response = await fetch("https://dangil-artisticsw.site/space/3661157737", {
+          method: "GET",
+          credentials: "include" // 쿠키 포함 설정
+        });
+  
+        const bookNameData = await response.json();
+        const bookNameList = bookNameData.data.user_space_data.book_list;
+        
+        if (bookNameList.length === 0){ // bookNameList의 길이 확인
+          createBook(bookName);
+          console.log('처음생성')
+        } else {
+          updateBook(bookName, "설명을 여기에 입력");
+          console.log('이미생성된기록있음')
+        }
+      } catch (error) {
+        console.error('Error handling the book name:', error);
+      }
     }
   };
+  // 책 이름 작성중일때 외부 클릭 차단 및 엔터 & esc로만 입력 완료 설정
+  useEffect(() => {
+    const handleDocumentClick = (event) => {
+      if (
+        NameEditing && 
+        inputRef.current && 
+        !inputRef.current.contains(event.target)) {
+        event.preventDefault();
+        event.stopPropagation(); // 클릭 이벤트 전파를 차단하여 외부 클릭 차단
+      }
+    };
+    // 문서에 클릭 이벤트 추가
+    document.addEventListener('click', handleDocumentClick, true);
+    // 컴포넌트가 언마운트되면 이벤트 리스너 제거
+    return () => {
+      document.removeEventListener('click', handleDocumentClick, true);
+    };
+  }, [NameEditing]);
 
-  const handleKeyDown = async (event) => {
+  // 책이름 관련 end
+  
+
+
+  // 책내용 저장
+  const bookTextChange = (event) => {
+    setNoteDescription(event.target.value);
+  };
+  // 책내용 엔터
+  const bookTextEnter = async (event) => {
     if (event.key === 'Enter' && !event.shiftKey) {  // Shift 키를 함께 누르지 않은 경우에만 작동
       event.preventDefault();  // 기본 엔터 동작 방지 (새 줄 추가 방지)
       const newNote = noteDescription.trim() + '\n';  // 현재 입력된 내용 끝에 개행 추가
@@ -67,8 +110,6 @@ const Book = ({editBook, setEditBook}) => {
       setNoteDescription(newNote);  // 입력 필드 업데이트 (새 줄 추가)
     }
   };
-
-
 // 백엔드로 데이터 전송
 const saveNoteDescription = async (description) => {
   try {
@@ -88,25 +129,6 @@ const saveNoteDescription = async (description) => {
     setPlusButton(!plusButton);
   }
 
-  const handleInputBlur = (e) => {
-    e.stopPropagation(); // 추가: 이벤트 전파를 멈춤
-     // 입력 값이 공백일 경우 이전 이름을 유지
-    if (bookName.trim().length === 0) {
-      setBookName(previousBookName); // previousBookName은 이전의 유효한 이름을 저장하는 상태
-      console.log('Book name restored to previous valid name.');
-    } else {
-      createBook(bookName);
-    }
-    setIsEditing(false);
-  };
-
-  //bookName이 유효한 값으로 변경될 때마다 previousBookName을 업데이트
-  useEffect(() => {
-    if (bookName.trim().length > 0) {
-      setPreviousBookName(bookName);
-    }
-  }, [bookName]);
-
   useEffect(() => {
     const handleClickOutside = (event) => {
       if (bookRef.current && !bookRef.current.contains(event.target)) {
@@ -120,22 +142,6 @@ const saveNoteDescription = async (description) => {
       document.removeEventListener("click", handleClickOutside);
     };
   }, []);
-
-
-  //책 이름 로드
-  // useEffect(() => {
-  //   const fetchBookName = async () => {
-  //     try {
-  //       const response = await fetch('/book/update');
-  //       if (!response.ok) throw new Error('Failed to fetch');
-  //       const data = await response.json();
-  //       setBookName(data.name);
-  //     } catch (error) {
-  //       console.error('Failed to fetch book name:', error);
-  //     }
-  //   };
-  //   fetchBookName();
-  // }, []);
 
   // 책 정보 업데이트
   const updateBook = async (newTitle, newDescription) => {
@@ -159,22 +165,23 @@ const saveNoteDescription = async (description) => {
     }
   };
   
-
   //책 생성시 백엔드에 저장
   const createBook = async (title) => {
     try {
-      const response = await fetch('/book/create', {
+      const response = await fetch('https://dangil-artisticsw.site/book/create', {
         method: 'POST',
+        credentials: "include", // 쿠키 포함
         headers: {
-          'Content-Type': 'application/json',
-        },
+        Cookie: "session_id=" + cookies.session_id,
+        'Content-Type': 'application/json'
+      },
         body: JSON.stringify({
           note_title: title,
-          note_description: 'Description here' // 추가 정보 입력
+          note_description: 'hi' // 추가 정보 입력
         })
       });
       const data = await response.json();
-      console.log('Book created:', data);
+      console.log(data);
     } catch (error) {
       console.error('Failed to create book:', error);
     }
@@ -182,7 +189,7 @@ const saveNoteDescription = async (description) => {
 
   return (
     <div ref={bookRef} >
-     <img src="/path/to/book.jpg" alt="Book Image" onClick={handleBookClick} style={{ cursor: 'pointer'}}/>
+     <img src="/path/to/book.jpg" alt="Book Image" onClick={bookImageClick} style={{ cursor: 'pointer'}}/>
         <div className={`book-page ${editBook ? 'visible': ''}`}>
           <div className="book-name-box">
             <div className="icon-box">
@@ -190,22 +197,22 @@ const saveNoteDescription = async (description) => {
               <div className="gray-colline"></div>
               <RiDownloadLine/>
               <FaRegFile/>
-              {/* <RxPencil1/> */}
               <LiaHighlighterSolid/>
               <LuEraser/>
             </div>
             <div className="book-name">
-                {isEditing ? (
-                  <input style={{textAlign:'center',  fontSize:'20px'}}
+                {NameEditing ? (
+                  <input
+                    ref={inputRef}  
+                    style={{textAlign:'center',  fontSize:'20px'}}
                     type="text"
                     value={bookName}
-                    onChange={handleInputChange}
-                    onKeyDown={handleInputKeyDown}
-                    onBlur={handleInputBlur}
+                    onChange={bookNameChange}
+                    onKeyDown={bookNameEnter}
                     autoFocus
                   />
                 ) : (
-                  <p onClick={handleEditClick} style={{ cursor: 'text', fontSize:'20px'}}>
+                  <p onClick={bookNameEdit} style={{ cursor: 'text', fontSize:'20px'}}>
                     {bookName}
                   </p>
                 )}
@@ -214,11 +221,13 @@ const saveNoteDescription = async (description) => {
           <div className="gray-rowline"></div>
           <div className="book-content">
               <textarea
+              ref={textareaRef}
               value={noteDescription}
-              onChange={handleChange}
-              onKeyDown={handleKeyDown}
+              onChange={bookTextChange}
+              onKeyDown={bookTextEnter}
               className="book-text"
               placeholder="원하는 글을 작성해주세요"
+              disabled={NameEditing}
             />
           </div>
         </div>
