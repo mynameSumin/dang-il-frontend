7a134efdcf599b6e45e7ad3fcc180f41ecec5ab3
ikjun <chj3123@naver.com>
2024-10-01 22:32:08 +0900

    feat : 책 색깔 변경 & 책 전체화면 기능 구현

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,16 +1,19 @@
 import React, { useState, useRef, useEffect, forwardRef } from 'react';
 import "../styles/Book.css";
-import { RiHomeLine } from "react-icons/ri";
-import { RiDownloadLine } from "react-icons/ri";
-import { LuEraser } from "react-icons/lu";
-import { LiaHighlighterSolid } from "react-icons/lia";
-import { FaRegFile } from "react-icons/fa6";
 import icon from "../assets/+ icon.png";
-import Ellipse from "../assets/Ellipse.png";
-import Vector from "../assets/Vector.png";
 import total from "../assets/total.png";
 import Lock from "../assets/Lock.png";
 import { useCookies } from "react-cookie";
+import X from "../assets/X.png";
+import circle from "../assets/circle.png";
+import home from "../assets/home.png";
+import download from "../assets/download.png";
+import eraser from "../assets/eraser.png";
+import lightpen from "../assets/lightpen.png";
+import newpage from "../assets/newpage.png";
+import T from "../assets/T.png";
+
+
 
 const Book = forwardRef(
   ({editBook, setEditBook, bookName, setBookName}, bookRef) => {
@@ -23,6 +26,7 @@ const Book = forwardRef(
 
   // +버튼 상태
   const [plusButton, setPlusButton] = useState(false);
+  // const [XButton, setXButton] = useState(false);
 
   // 책이름 관련 start
 
@@ -35,6 +39,7 @@ const Book = forwardRef(
   const bookNameEdit = (e) => {
     e.stopPropagation();
     setNameEditing(true);
+    console.log("편집 모드 활성화");
   };
   // 책이름 변경될때마다 호출되는 함수
   const bookNameChange = (e) => {
@@ -47,6 +52,7 @@ const Book = forwardRef(
     if (bookName === "") return; // bookName이 비어있으면 함수 종료
     if (e.key === 'Enter' || e.key === 'Escape' /*Escape은 Esc를 의미함*/) {
       setNameEditing(false); // 편집 상태 종료
+      console.log("편집 모드 종료");
       // if (빈배열) {
       //   createBook()
       // }
@@ -76,6 +82,9 @@ const Book = forwardRef(
     }
   };
 
+
+
+
   // 책 정보 업데이트
   const updateBook = async (newTitle, newDescription) => {
     try {
@@ -98,6 +107,13 @@ const Book = forwardRef(
     }
   };
 
+
+// 책이 열릴 때 초기 상태로 리셋
+useEffect(() => {
+  if (editBook) {
+    setPlusButton(false);
+    }
+}, [editBook]); // 의존성 배열에 isListVisible 추가
 
   
   // 책 이름 작성중일때 외부 클릭 차단 및 엔터 & esc로만 입력 완료 설정
@@ -157,10 +173,11 @@ const Book = forwardRef(
     }
   };
 
-  const plusMenu = (e) => {
-    e.stopPropagation()
-    setPlusButton(!plusButton);
-  }
+  // side 버튼 활성화될때 circle 클릭이 되는걸 막음.
+  const circleClick = (e) => {
+    e.stopPropagation(); 
+    console.log('Circle clicked'); 
+  };
 
   //책 생성시 백엔드에 저장
   const createBook = async (title) => {
@@ -183,19 +200,87 @@ const Book = forwardRef(
       console.error('Failed to create book:', error);
     }
   };
+  //책 색깔 관련
+  const [bookColor, setBookColor] = useState('')
+   // 선택된 색상으로 상태 업데이트
+  const changeBookColor = (color) => {
+    setBookColor(color);
+  };
+
+
+  const PlusButtonClick = (e) => {
+    e.stopPropagation(); 
+    setPlusButton(current => !current);
+  };
+
+
+  //책 전체화면 관련
+  const [isFullscreen, setIsFullscreen] = useState(false);
+  const totalClick = (e) => {
+    e.stopPropagation();
+    setIsFullscreen(true);
+  };
+  //전체화면일때 esc누르면 전체화면 해제됨
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (e.key === 'Escape') {
+        setIsFullscreen(false);
+      }
+    };
+    document.addEventListener('keydown', handleKeyDown);
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
+  //전체화면에서 가이드문구 5초뒤에 사라짐
+  const [showGuideEsc, setShowGuideEsc] = useState(true);
+  useEffect(() => {
+    if (isFullscreen) {
+      setShowGuideEsc(true); 
+      const timer = setTimeout(() => {
+        setShowGuideEsc(false); // 3초 뒤에 가이드를 숨김
+      }, 3000);
+
+      return () => clearTimeout(timer); // 컴포넌트 언마운트 시 타이머 제거
+    }
+  }, [isFullscreen]);
+
+
+
+  const backDefaultBook = (e) => {
+    e.stopPropagation();
+    setIsFullscreen(false);
+  }
+
+  const LockClick = (e) => {
+    e.stopPropagation(); 
+  };
+
+    // `editBook` 상태가 변경될 때마다 `plusButton`을 초기화
+    useEffect(() => {
+      if (editBook) {
+        setPlusButton(false); // `editBook`이 true일 때 `plusButton` 초기화
+        setIsFullscreen(false);
+      }
+    }, [editBook]);
 
   return (
     
     <div bookRef={bookRef}>
-        <div bookRef={bookRef} onClick={(e) => e.stopPropagation()} className={`book-page ${editBook ? 'visible': ''}`}>
+        <div 
+          bookRef={bookRef} 
+          onClick={(e) => e.stopPropagation()} 
+          className={`book-page ${editBook ? 'visible' : ''} ${isFullscreen ? 'fullscreen' : ''}`}
+          style={{ backgroundColor: bookColor }}>
           <div className="book-name-box">
             <div className="icon-box">
-              <RiHomeLine onClick={HomeButton} style={{marginLeft: "20", marginRight: "2"}}/>
+              <img src={home} onClick={HomeButton} className="home"/>
               <div className="gray-colline"></div>
-              <RiDownloadLine/>
-              <FaRegFile/>
-              <LiaHighlighterSolid/>
-              <LuEraser/>
+              <img src={download}/>
+              <img src={newpage}/>
+              <img src={T}/>
+              <img src={lightpen}/>
+              <img src={eraser}/>
             </div>
             <div className="book-name">
                 {NameEditing ? (
@@ -206,18 +291,26 @@ const Book = forwardRef(
                     onChange={bookNameChange}
                     onKeyDown={bookNameEnter}
                     autoFocus
+                    style={{ backgroundColor: bookColor }}
                   />
                 ) : (
-                  <p onClick={bookNameEdit} style={{ cursor: 'text', fontSize:'20px'}}>
+                  <p 
+                  onClick={bookNameEdit} 
+                  style={{ cursor: 'text', fontSize:'20px', backgroundColor: bookColor}}>
                     {bookName}
                   </p>
                 )}
             </div>
             <div className="bookColors">
-                <div className="color1"></div>
-                <div className="color2"></div>
-                <div className="color3"></div>
-                <div className="color4"></div>
+                <div className="color1" onClick={() => changeBookColor('#D3DEEE')}></div>
+                <div className="color2" onClick={() => changeBookColor('#B3C7EC')}></div>
+                <div className="color3" onClick={() => changeBookColor('#D8C7D4')}></div>
+                <div className="color4" onClick={() => changeBookColor('#DDD4EF')}></div>
+                {isFullscreen && (
+                  <div>
+                    <img  onClick={backDefaultBook} src={X}/>    
+                  </div>
+                )}
             </div>
           </div>     
           <div className="gray-rowline"></div>
@@ -228,30 +321,44 @@ const Book = forwardRef(
               onChange={bookTextChange}
               onKeyDown={bookTextEnter}
               className="book-text"
-              placeholder="원하는 글을 작성해주세요"
+              // placeholder="원하는 글을 작성해주세요"
               disabled={NameEditing}
             />
           </div>
-        </div>
-        <img className="plus-icon" onClick={plusMenu} src={icon} id="+ icon" />
-        
-        {plusButton && (
-        <div className="side-buttons">
-          <div class="circle">
-            <img className="side-Vector" src={Vector} id='Vector'></img>
-          </div>
 
-          {/* <div>
-            <img className="side-button-ellipse2" src={Ellipse} id='Ellipse'></img>
-            <img className="total" src={total} id='total'></img>
-          </div>
-
-          <div>
-            <img className="side-button-ellipse3" src={Ellipse} id='Ellipse'></img>
-            <img className="Lock" src={Lock} id='Lock'></img>
-          </div> */}
+          {isFullscreen && showGuideEsc && (
+            <div className="guideEsc">
+              <p>전체화면을 해제하시려면  <span className="Esc">Esc</span> 를 눌러주세요.
+              </p>
+            </div>  
+          )}
         </div>
-      )}
+
+        {editBook && (
+            <>
+            <div>
+              {!plusButton && (
+                <img className="plus-icon" onClick={PlusButtonClick} src={icon} alt="+ icon"/>
+              )}
+
+              {plusButton && (
+                <div className="side-buttons">
+                  <div>
+                    <img className="circleTop" onClick={circleClick} src={circle} alt="circle"/>
+                    <img className="circleMid" onClick={circleClick} src={circle} alt="circle"/>
+                    <img className="circleBottom" onClick={circleClick} src={circle} alt="circle"/>
+                  </div>
+
+                  <div className="clickbox">
+                    <img className="Lock" onClick={LockClick} src={Lock} alt="Lock icon"/>
+                    <img className="total" onClick={totalClick} src={total} alt="Overlay icon"/>
+                    <img className="X" onClick={PlusButtonClick} src={X} alt="Overlay icon"/>
+                  </div>
+                </div>
+              )}
+              </div>
+            </>
+          )}
     </div>
   );
 });
